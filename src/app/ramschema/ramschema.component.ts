e92795b1d704f8ff804925fa098b0bfc1bbcc960
Maamoun Okla <mamoun95bo@icloud.com>
2024-05-14 03:15:29 +0200

    cleaning code, deploy  ready

--- src/app/ramschema/ramschema.component.ts
+++ src/app/ramschema/ramschema.component.ts
@@ -11,21 +11,18 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './ramschema.component.css',
 })
 export class RamschemaComponent {
-  courseList : Course[] = [];
-  filteredCourseList : Course[] = [];
-  searchText: string = "";
+  courseList: Course[] = [];
+  filteredCourseList: Course[] = [];
+  searchText: string = '';
   sortedData: Course[] = [];
- 
 
-  constructor(private courseservice : CourseService){
-   
-  }
+  constructor(private courseservice: CourseService) {}
   ngOnInit() {
-    //fetch courses and add them to course list on initilize. 
-    this.courseservice.getCourses().subscribe(data => {
+    //fetch courses and add them to course list on initilize.
+    this.courseservice.getCourses().subscribe((data) => {
       this.courseList = data;
       this.filteredCourseList = data;
-    })
+    });
   }
   filterInput() {
     //if empty string or only white space, display all existing courses.
@@ -33,21 +30,24 @@ export class RamschemaComponent {
       this.filteredCourseList = [...this.courseList];
       return;
     }
-   //filter courses
-   
-   this.filteredCourseList = this.courseList.filter( (course) => 
-    course.code.toLocaleLowerCase().includes(this.searchText.toLowerCase()) ||
-    course.coursename.toLocaleLowerCase().includes(this.searchText.toLocaleLowerCase())
-   );
-   console.log(this.filteredCourseList);
+    //filter courses
+
+    this.filteredCourseList = this.courseList.filter(
+      (course) =>
+        course.code
+          .toLocaleLowerCase()
+          .includes(this.searchText.toLowerCase()) ||
+        course.coursename
+          .toLocaleLowerCase()
+          .includes(this.searchText.toLocaleLowerCase())
+    );
+    console.log(this.filteredCourseList);
   }
 
-  sortCourses(dataType : keyof Course) {
-    this.courseList.sort()
-
+  sortCourses(dataType: keyof Course) {
     this.sortedData = this.filteredCourseList.sort((a, b) => {
-      if (a[dataType] > b[dataType]) { return 1};
-      if (a[dataType] < b[dataType]) {return -1};
+      if (a[dataType] > b[dataType]) { return 1;  }
+      if (a[dataType] < b[dataType]) { return -1; }
       return 0;
     });
   }
